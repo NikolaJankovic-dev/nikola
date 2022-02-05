74c070013797230a3ec43c8a5b11b9451c8070cd
Nikola Jankovic <dzoni6usb@gmail.com>
2022-02-05 23:33:46 +0100

    contact form added

---
+++ src/components/ContactFrom/ContactForm.jsx
@@ -0,0 +1,83 @@
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import style from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+  return (
+    <div>
+      <form className={style.form} onSubmit={handleSubmit} >
+        <div className={style.formCont}>
+        <h4>LET'S TALK!</h4>
+      <p>I will get back to you within one to two days through email. Also please don't forget to check your spam account just in case!</p>
+          <div>
+            {" "}
+            <TextField
+              label="Name"
+              name="name"
+              required
+              id="name"
+              variant="standard"
+              className={style.inputs}
+              sx={{ width: "100%" }}
+
+            />
+          </div>
+          <div>
+            <TextField
+              label="E-mail"
+              name="email"
+              required
+              id="name"
+              variant="standard"
+              className={style.inputs}
+              sx={{ width: "100%" }}
+
+            />
+          </div>
+          <div>
+            <TextField
+              multiline
+              label="Message"
+              id="message"
+              required
+              variant="standard"
+              minRows={4}
+              className={style.inputs}
+              // my={4}
+              sx={{ width: "100%" }}
+              margin="1000px"
+            />
+            <div>
+              <Button type="submit" variant="contained" className={style.send}>
+                SEND REQUEST
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/components/Phase/Phase4.jsx
+++ src/components/Phase/Phase4.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import style from './Phase4.module.css'
+import LightSaber from "./LightSaber/LightSaber";
+import ContactForm from '../ContactFrom/ContactForm';
 
-const Phase4 = ({phase}) => {
-  return <div className={style.phase4}>Phase4</div>;
+
+const Phase4 = ({phase, setPhase, page, setPage}) => {
+  return (
+    <div>
+    <div className={!page ? style.phase2 : style.phase5}>
+      
+      <div className={!page ? style.phase2contactive : style.phase2cont}>
+        <h1>
+          
+          CONTACT ME
+        </h1>
+       
+        <ContactForm/>
+        
+      </div>
+      <div className={page ? style.imageDivPage : style.imageDiv}></div> 
+    </div>
+    </div>
+  );
 };
 
 export default Phase4;
