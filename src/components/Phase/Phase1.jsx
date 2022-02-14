058fba4c65e9a5c56c38b2e099ffd08f8e2dc9ef
Nikola Jankovic <dzoni6usb@gmail.com>
2022-02-14 17:10:55 +0100

    deathstar animation

--- src/components/Phase/Phase1.jsx
+++ src/components/Phase/Phase1.jsx
@@ -1,23 +1,58 @@
 import React from "react";
+import { Suspense, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import LightSaber from "./LightSaber/LightSaber";
 import style from "./Phase1.module.css";
 
-const Phase1 = ({phase, setPhase, page, setPage}) => {
+function DeathStar({ ...props }) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/star.glb");
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.DeathStar.geometry}
+        material={materials.DeathStar}
+        position={[0, 0, 0]}
+        scale={2.5}
+      />
+      <OrbitControls
+        enableZoom={false}
+        enablePan={false}
+        enableRotate={true}
+        zoomSpeed={0.6}
+        autoRotate={true}
+      />
+    </group>
+  );
+}
+
+const Phase1 = ({ phase, setPhase, page, setPage }) => {
   return (
     <div>
-    <div className={style.phase2}>
-      
-      <div className={style.phase2cont}>
-        <h1>
-          
-          Portfolio
-        </h1>
-        <LightSaber />
-        
-        <p>Frontend Developer</p>
+      <div className={style.phase2}>
+        <div className={style.phase2cont}>
+          <h1>Portfolio</h1>
+          <LightSaber />
+
+          <p>Frontend Developer</p>
+        </div>
+        <div className={style.imageDiv}>
+          <Canvas>
+            {/* <ambientLight intensity={0.5} /> */}
+            <pointLight
+              color="#9cc8ee;"
+              position={[55, 50, 1]}
+              intensity={0.8}
+            />
+
+            <Suspense fallback={null}>
+              <DeathStar />
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
-      <div className={style.imageDiv}></div> 
-    </div>
     </div>
   );
 };
